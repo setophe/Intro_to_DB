f830bc13d9a9d92201763ed9707e2725236beac5
Mostafa <elbayemoustapha@gmail.com>
2025-10-27 22:45:05 +0100

    tasks 5 and 6 done

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- Script: task_5.sql
+-- Description: Insert a single row into the customer table in the alx_book_store database
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
