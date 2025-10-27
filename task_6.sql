dcc7f163a8f58769b02ad06683d6b87ddee5a7fa
Mostafa <elbayemoustapha@gmail.com>
2025-10-27 22:46:32 +0100

    tasks 5 and 6 done

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,5 @@
--- Script: task_6.sql
--- Description: Insert multiple rows into the customer table in the alx_book_store database
-
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-
